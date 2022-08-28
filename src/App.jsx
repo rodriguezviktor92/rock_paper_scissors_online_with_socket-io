12dd4972dafb4cfb6ab538cd7eaa5deac4ddc29b
rodriguezviktor92 <rodriguezviktor@gmail.com>
2022-08-28 18:17:48 -0500

    feat(App.jsx, package.json, .gitignore): add socket.io-client

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import io from 'socket.io-client';
 import './index.css';
 import bgTriangle from './assets/bg-triangle.svg';
 import iconPaper from './assets/icon-paper.svg';
@@ -8,8 +9,26 @@ import { Option } from './components/Options';
 import { Header } from './components/Header';
 import Modal from './components/Modal';
 
+//Local
+const socket = io('http://localhost:4000');
+
+//Remote
+//const socket = io();
+
 function App() {
-  const [count, setCount] = useState(0);
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    const receiverMessage = (message) => {
+      setMessages([...messages, message]);
+    };
+
+    socket.on('message', receiverMessage);
+
+    return () => {
+      socket.off('message', receiverMessage);
+    };
+  }, [messages]);
 
   return (
     <div className="w-screen h-screen bg-gradient-to-r from-background to-background2 p-10 grid justify-center items-center grid-cols-options">
