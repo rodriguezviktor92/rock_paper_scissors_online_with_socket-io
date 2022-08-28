e785befc889f9ea50fb1d101abf386aeffd645ad
rodriguezviktor92 <rodriguezviktor@gmail.com>
2022-08-28 11:27:17 -0500

    feat(*): add options

---
+++ src/components/Options/index.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+
+export function Option({ image, type }) {
+  const options = {
+    paper: {
+      bg: 'bg-paper',
+      positionx: 'top-0',
+      positiony: 'justify-start',
+    },
+    rock: {
+      bg: 'bg-rock',
+      positionx: 'bottom-0',
+      positiony: 'justify-center',
+    },
+    scissors: {
+      bg: 'bg-scissors',
+      positionx: 'top-0',
+      positiony: 'justify-end',
+    },
+  };
+
+  return (
+    <div
+      className={`flex absolute w-full ${options[type].positionx} ${options[type].positiony}`}
+    >
+      <span
+        className={`rounded-full p-4 ${options[type].bg} shadow-shadow-out`}
+      >
+        <span
+          className="w-[100px] h-[100px] bg-white rounded-full flex 
+ justify-center items-center shadow-shadow-in"
+        >
+          <img src={image} />
+        </span>
+      </span>
+    </div>
+  );
+}
