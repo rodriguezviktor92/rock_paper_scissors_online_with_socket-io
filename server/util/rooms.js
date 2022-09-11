104936097e1f07e16cb59fb317f2acbc3cb463be
victor.rodriguez4.contractor <victor.rodriguez4.contractor@bbva.com>
2022-09-11 14:05:42 -0500

    feat(server/index.js): update exitRoom and add deleteRoom

--- server/util/rooms.js
+++ server/util/rooms.js
@@ -9,9 +9,16 @@ export const joinRoom = (roomId, player2Id) => {
 };
 
 export const exitRoom = (roomId, player) => {
-  if (player == 1) {
-    delete rooms[roomId];
+  if (player === 1) {
+    rooms[roomId][0] = '';
   } else {
     rooms[roomId][1] = '';
   }
+  deleteRoom(roomId);
 };
+
+const deleteRoom = (roomId)=>{
+  if(rooms[roomId][0] === '' && rooms[roomId][1] === ''){
+    delete rooms[roomId];
+  }
+}
